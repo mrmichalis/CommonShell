5ab50e96a9c140f14323d654f346d268e038a2a6
Michalis K <mihaliz@gmail.com>
2011-12-12 14:43:47 +0000

    Result to file

--- scripts/portscan.py
+++ scripts/portscan.py
@@ -2,23 +2,36 @@
 # --------------------------------------------------------------------------
 #
 # 12/12/2011 Michalis
-#
-# used to scan IP and ports 
+# quick script to perform port scanning
+# used to scan IP and ports
 # Requires file "ip.lst" and "ports.lst"
-#
+# 
 # --------------------------------------------------------------------------
 from socket import *
+from optparse import OptionParser
 
 if __name__ == '__main__':
+  parser = OptionParser()
+  parser.add_option("-f", "--file", dest="filename",
+                  help="write report to FILE", metavar="FILE")
+  (options, args) = parser.parse_args()
 
-  for target in open('ip.lst', 'r'):
+  print "Saving to: %s..." % options.filename
+  FILE = open(str(options.filename),"w")
+
+  for target in open('smart-ip.lst', 'r'):
     targetIP = gethostbyname(target)
-    print 'Starting scan on host %s ' % (targetIP,) 
-    for port in open('ports.lst', 'r'):
+    print 'Starting scan on host %s' % (targetIP,)
+    FILE.writelines('Starting scan on host %s\n' % (targetIP,))
+    for port in open('smart-ports.lst', 'r'):
       s = socket(AF_INET, SOCK_STREAM)
       result = s.connect_ex((targetIP, int(port)))
       if(result == 0) :
-          print 'Port %d: OPEN' % (int(port),)
+        print 'Port %d: OPEN' % (int(port),)
+        FILE.writelines('Port %d: OPEN\n' % (int(port),))
       else:
-          print 'Port %d: CLOSE' % (int(port),)
+        print 'Port %d: CLOSE' % (int(port),)
+        FILE.writelines('Port %d: CLOSE\n' % (int(port),))
       s.close()
+
+FILE.close()
