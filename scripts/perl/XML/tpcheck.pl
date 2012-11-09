be70030cff6823cab105486f7da75c2c15bd2fc8
Michalis™ <mihaliz@gmail.com>
2012-11-09 16:36:24 +0000

    some corrections

--- scripts/perl/XML/tpcheck.pl
+++ scripts/perl/XML/tpcheck.pl
@@ -4,10 +4,10 @@ use XML::Twig;
 use LWP;
 
 my $logfile = "./mylogfile.log"; # log file location
-my $out_message = ""; 			 # init log file
+my $out_message = ""; 			 # init log message
 # the URL for the xml to parse
 my $url		= 'http://sftp.uk3.ribob01.net:8080/publish-notification/zabbixThreadMonitor.jsp';
-my $twig 	= XML::Twig->parse( TwigHandlers => { 'thread[string(isRunning)="false"]' => \&process}, $url);
+my $twig 	= XML::Twig->parse( TwigHandlers => { 'thread[string(isRunning)="true"]' => \&process}, $url);
 
 # local
 # my $twig 	= new XML::Twig( TwigHandlers => { 'thread[string(isRunning)="false"]' => \&process });  
@@ -32,7 +32,7 @@ sub process {
 sub log {
 	my $message = shift;
     my $now     = localtime;
-	my $logmsg 	= "$now ERROR $message";
+	my $logmsg 	= "$now ERROR\t$message";
 	open LOGFILE, ">>$logfile" or die "cannot open logfile $logfile for append: $!";
 	print LOGFILE $logmsg;
 	close LOGFILE;
