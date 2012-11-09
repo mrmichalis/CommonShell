54d69de7753f587efefcffd836b5cd50b603a50a
Michalis™ <mihaliz@gmail.com>
2012-11-09 16:18:07 +0000

    minify

--- scripts/perl/XML/tpcheck.pl
+++ scripts/perl/XML/tpcheck.pl
@@ -3,22 +3,37 @@ use strict;
 use XML::Twig;
 use LWP;
 
-# my $url	= 'http://sftp.uk3.ribob01.net:8080/publish-notification/zabbixThreadMonitor.jsp';
-# my $twig 	= XML::Twig->parse( TwigHandlers => { 'thread[string(isRunning)="true"]' => \&process}, $url);
+my $logfile = "./mylogfile.log"; # log file location
+my $out_message = ""; 			 # init log file
+# the URL for the xml to parse
+my $url		= 'http://sftp.uk3.ribob01.net:8080/publish-notification/zabbixThreadMonitor.jsp';
+my $twig 	= XML::Twig->parse( TwigHandlers => { 'thread[string(isRunning)="false"]' => \&process}, $url);
 
-my $twig= new XML::Twig( TwigHandlers => { 'thread[string(isRunning)="false"]' => \&process });  
-$twig->parsefile( "test.xml");
+# local
+# my $twig 	= new XML::Twig( TwigHandlers => { 'thread[string(isRunning)="false"]' => \&process });  
+# $twig->parsefile( "test.xml");
 
-sub process
-{
+# process xml file
+sub process {
 	my ($twig, $element) = @_;
 	#print $element->name,"\n";
 	foreach my $child ($element->children)
 	{
-		if ($child->name !~ m/last/)
+		if ($child->name !~ /last/)
 		{
-			print $element->parent->name," ",$child->name,": ",$child->text," ";
+			$out_message .= "\t".$child->name.": ".$child->text."\t";
 		}
 	}
-	print "\n";
+	&log ($element->parent->name." $out_message\n");
+	undef $out_message;
+}
+
+# write to a log file
+sub log {
+	my $message = shift;
+    my $now     = localtime;
+	my $logmsg 	= "$now ERROR $message";
+	open LOGFILE, ">>$logfile" or die "cannot open logfile $logfile for append: $!";
+	print LOGFILE $logmsg;
+	close LOGFILE;
 }
